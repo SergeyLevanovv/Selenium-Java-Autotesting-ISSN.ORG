cc5e1bc1f83f646f92803686dcc4a32e51c3f45e
SergeyLevanovv <ser.lewanow@gmail.com>
2024-08-08 20:07:35 +0400

    initial commit.

---
+++ src/test/java/com/issn/org/selenium_java_autotesting_issn_org/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.issn.org.selenium_java_autotesting_issn_org;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
