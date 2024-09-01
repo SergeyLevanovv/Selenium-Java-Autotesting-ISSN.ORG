4e724eb4ff3b54fcf45766d98ea9826a767a0ece
SergeyLevanovv <ser.lewanow@gmail.com>
2024-09-02 03:22:31 +0400

    initial commit.

---
+++ src/main/java/com/issn/org/seleniumjavatesting/SeleniumJavaTestingApplication.java
@@ -0,0 +1,13 @@
+package com.issn.org.seleniumjavatesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeleniumJavaTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeleniumJavaTestingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/issn/org/seleniumjavatesting/SeleniumJavaTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.issn.org.seleniumjavatesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeleniumJavaTestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
